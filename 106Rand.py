de3c4847749066993b04319bd1a7c84e3f4ef48a
MohammedAbdulAhadSaud <149686654+MohammedAbdulAhadSaud@users.noreply.github.com>
2025-01-09 19:11:36 +0530

    Update 106Rand.py

--- 106Rand.py
+++ 106Rand.py
@@ -1,9 +1,8 @@
 import random
 num = random.randrange(1, 10)
 print( num )
-num = random.randrange(1, 10, 2)
-print( num )
-num = random.randrange(0, 101, 10)
-print( num ) 
+a_list = [34, 23, 65, 86, 23, 43]
+random.shuffle( a_list )
+print( a_list ) 
 num=random.choice("Python Programming Lab")
 print(num)
