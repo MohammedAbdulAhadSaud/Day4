f4d7bdd68e81e2cf7392962ed0802cf55be00944
MohammedAbdulAhadSaud <149686654+MohammedAbdulAhadSaud@users.noreply.github.com>
2025-01-09 19:08:54 +0530

    Create 106Rand.py

---
+++ 106Rand.py
@@ -0,0 +1,9 @@
+import random
+num = random.randrange(1, 10)
+print( num )
+num = random.randrange(1, 10, 2)
+print( num )
+num = random.randrange(0, 101, 10)
+print( num ) 
+num=random.choice("Python Programming Lab")
+print(num)
